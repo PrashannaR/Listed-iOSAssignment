ea1f78eeb93b6d30a57b390235bdae15c45640fe
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-07-07 16:00:36 +0530

    feat: initial commit

---
+++ Listed-iOSAssignment/Listed_iOSAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Listed_iOSAssignmentApp.swift
+//  Listed-iOSAssignment
+//
+//  Created by Prashanna Rajbhandari on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Listed_iOSAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
